c68f63b1d18a6a079ffca4b939ee4fee008683fd
Damir <d.makhmudov@kazan.csoft.ru>
2022-04-22 16:02:34 +0300

    Declension block is done

--- README.md
+++ README.md
@@ -5,14 +5,18 @@ Server side solution for Pereplan company
 
 ## pereplan api js
 https://cdn.jsdelivr.net/gh/DamirMakhmudov/pereplan/engine.js
+- Склонение : перевод в дательный падеж
 - ИНН : все данные по ИНН
 - Код подразделения : адрес выдачи паспорта по коду подразделения
 - Фамилия И. О. : короткое ФИО
 - БИК : данные банка по БИК
-- Дательный падеж : перевод в дательный падеж
 
 ## pereplan api gs
 https://script.google.com/macros/s/AKfycbw0kKa-yHVMcrWyihYHaqNQh1FvvdG5hHqmZyjvJEddZxwtJwoTc4EzHBNsiW_5neu4/exec
 - Проверка и создание папки на сервере
 
 ## pereplan library gs
+- getData: получает данные записанные в JSON по ключу и листу
+- grabRange: возвращает диапазон из таблицы по листу и заголовку
+- checkFolder: проверяет папку на диске и в Таблице, создат если нет. Возвращает url папки
+- createFolder: создает папку на диске и подпапки
--- engine.js
+++ engine.js
@@ -1,17 +1,20 @@
-/**
- * Return diffetent forms of input value: genitive, dative etc
-*/
-// 5ecd5111-ffbf-4f9a-80e1-92f609c95aad damir
-// f8610d17-6d77-4756-83c8-ab21d0b7a6b0 pereplan
-// 287c2716-c7f5-4f2f-90cf-252881164284 csoft
-// https://ws3.morpher.ru/russian/spell?n=235&unit=рубль (ukrainian/spell)
 
-/* Example to run */
-function getM() {
-  getDeclensionLimit().then(data => {
+
+/**
+ *  Example functon illustrating how to use 'getDeslension' function
+ */
+function exampleDeclension() {
+  /*
+  5ecd5111-ffbf-4f9a-80e1-92f609c95aad damir
+  f8610d17-6d77-4756-83c8-ab21d0b7a6b0 pereplan
+  287c2716-c7f5-4f2f-90cf-252881164284 csoft
+  https://ws3.morpher.ru/russian/spell?n=235&unit=рубль (ukrainian/spell)
+  */
+  let token = '287c2716-c7f5-4f2f-90cf-252881164284'
+  getDeclensionLimit(token).then(data => {
     if (data > 0) {
       console.log(data);
-      getDeclension().then(data => {
+      getDeclension(token, 'Петр', 'Р').then(data => {
         if (data.status == 200) {
           console.log('dat', data)
         } else {
@@ -22,17 +25,13 @@ function getM() {
   });
 }
 
-async function getDeclensionLimit(token = '287c2716-c7f5-4f2f-90cf-252881164284') {
-  let url = `https://ws3.morpher.ru/get-queries-left?format=json&token=${token}`;
-  let response = await fetch(url, {
-    method: "GET",
-    headers: {
-      'Accept': 'application/json',
-    }
-  }).then(data => data.json())
-  return response;
-}
-
+/**
+ * Return declension form of input
+ * @param {string} token credential token to access
+ * @param {string} value input text to declen
+ * @param {string} format case for transform input value
+ * @return {object} object contains status and output value
+*/
 async function getDeclension(token = '287c2716-c7f5-4f2f-90cf-252881164284', value = 'Иван', format = 'Р') {
   let result = { status: 200, value: "" };
   let url = `https://ws3.morpher.ru/russian/declension?format=json&s=${value}&token=${token}`;
@@ -44,4 +43,20 @@ async function getDeclension(token = '287c2716-c7f5-4f2f-90cf-252881164284', val
   });
   result.status == 200 ? result.value = response[format] : result.value = response;
   return result
+}
+
+/**
+ * Return limit of declension service
+ * @param {string} token credential token to access
+ * @return {number} number of requests left per day
+*/
+async function getDeclensionLimit(token = '287c2716-c7f5-4f2f-90cf-252881164284') {
+  let url = `https://ws3.morpher.ru/get-queries-left?format=json&token=${token}`;
+  let response = await fetch(url, {
+    method: "GET",
+    headers: {
+      'Accept': 'application/json',
+    }
+  }).then(data => data.json())
+  return response;
 }
